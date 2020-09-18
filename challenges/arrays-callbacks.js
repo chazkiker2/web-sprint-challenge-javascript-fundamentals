cb80c0de865e293992ec033e9a8566bd23800c7d
Chaz Kiker <chazkiker@gmail.com>
2020-09-18 11:50:09 -0400

    finish all callback tasks in arrays-callbacks.js

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -61,12 +61,15 @@ console.log(lowPopulationAnimals);
 
 /* 
 * Request 4: .reduce() 
-TODO: UNFINISHED
+? DRAFT
+>> UNTESTED
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. 
+Find the total population from all the zoos using the .reduce() method. 
+Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+let populationTotal = zooAnimals.reduce( ((acc, it) => acc + it.population), 0);
 console.log(populationTotal);
 
 
@@ -74,30 +77,38 @@ console.log(populationTotal);
 
 /* 
 * Step 1: Create a higher-order function
-TODO: UNFINISHED
+? DRAFT
+>> UNTESTED
+  
   * Create a higher-order function named consume with 3 parameters: a, b and cb
   * The first two parameters can take any argument (we can pass any value as argument)
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+const consume = (a, b, cb) => cb(a, b);
 
 
 /* 
 * Step 2: Create several functions to callback with consume();
-TODO: UNFINISHED
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+* FINISHED
+>>TESTED  AND PASSED 
+  
+Create a function named add that returns the sum of two numbers
+Create a function named multiply that returns the product of two numbers 
+Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = (num1, num2) => num1 + num2;
+const multiply = (num1, num2) => num1 * num2;
+const greeting = (first, last) => `Hello ${first} ${last}, nice to meet you!`;
 
 
 /* 
 * Step 3: Check your work by un-commenting the following calls to consume(): 
-TODO: UNFINISHED
+>> ALL FOUR METHODS ABOVE TESTED AND PASSED
 */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
